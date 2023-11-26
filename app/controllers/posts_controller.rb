248b4a0c5e1e87e987237681e372f80017bf8c9f
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-11-25 21:30:00 -0500

    Add Update users_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
-end
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+    end
+  end
