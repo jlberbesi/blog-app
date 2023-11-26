ccdbbbb580016bfb317198edd588d67dea769a36
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-11-25 23:59:08 -0500

    Add corrections suggested y code reviewer

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe Like, type: :model do
   let!(:post) { Post.create!(title: 'Test Post', author: user, comments_counter: 0, likes_counter: 0) }
 
   it 'increments the likes counter when created' do
-    expect { Like.create(user: user, post: post) }.to change { post.reload.likes_counter }.by(1)
+    expect { Like.create(user:, post:) }.to change { post.reload.likes_counter }.by(1)
   end
 
   it 'decrements the likes counter when destroyed' do
-    like = Like.create(user: user, post: post)
+    like = Like.create(user:, post:)
     expect { like.destroy }.to change { post.reload.likes_counter }.by(-1)
   end
 
   describe '#update_likes_counter' do
     it 'updates the likes counter of the post' do
-      like = Like.create(user: user, post: post)
+      like = Like.create(user:, post:)
       expect { like.destroy }.to change { post.reload.likes_counter }.by(-1)
     end
   end
