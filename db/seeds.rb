9a75cfc2355c5c09546f2fe8aa5ec842d2c7f0d5
Luis Emilio Rojas <88105477+jlberbesi@users.noreply.github.com>
2023-12-02 19:20:43 -0500

    Add code for excersise

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@
 third_user = User.create(name: 'Jose', photo: 'https://vivolabs.es/wp-content/uploads/2022/03/perfil-hombre-vivo.png', bio: 'Teacher from Poland.')
 
 https://vivolabs.es/wp-content/uploads/2022/03/perfil-hombre-vivo.png
+
+fourth_user = User.create(name: 'patricia', photo: 'https://b2472105.smushcdn.com/2472105/wp-content/uploads/2022/11/Fotografia-de-Retrato-18-1170x1170.jpg?lossy=1&strip=1&webp=1', bio: 'Teacher from Poland.')
+
+fifth_user = User.create(name: 'Andrea', photo: 'https://b2472105.smushcdn.com/2472105/wp-content/uploads/2022/11/Fotografia-de-Retrato-18-1170x1170.jpg?lossy=1&strip=1&webp=1', bio: 'Teacher from Poland.')
